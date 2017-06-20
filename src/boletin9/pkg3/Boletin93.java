41d47848a2cfb79e18836a43ec90a2c1a1a5a7db
propa <propa@DESKTOP-10ADTBO>
2017-06-20 15:23:44 +0200

    Acabado

---
+++ src/boletin9/pkg3/Boletin93.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin9.pkg3;
+
+/**
+ *
+ * @author propa
+ */
+public class Boletin93 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Area area1=new Area();
+        area1.calcularArea();
+    }
+    
+}
